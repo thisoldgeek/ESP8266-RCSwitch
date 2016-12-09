c4b25d5c2e6c576c7fcb1220e5145b33378cd7ad
Sujay Phadke <electronicsguy@users.noreply.github.com>
2016-12-10 00:17:25 +0530

    updated interface. ignore favicon requests

--- esp8266_rcswitch.ino
+++ esp8266_rcswitch.ino
@@ -6,14 +6,13 @@
  *  server_ip is the IP address of the ESP8266 module, will be 
  *  printed to Serial when the module is connected.
  */
-#include <RCSwitch.h>
+#include "RCSwitch.h"
 #include <ESP8266WiFi.h>
 
 RCSwitch mySwitch = RCSwitch();
 
-
 const char* ssid = "SSID";
-const char* password = "PASSWD";
+const char* password = "PASSWORD";
 
 // Create an instance of the server
 // specify the port to listen on as an argument
@@ -26,14 +25,11 @@ void setup() {
   // prepare GPIO2
   pinMode(2, OUTPUT);
   
-  
-  
   // Transmitter is connected to ESP8266 PIN #2
   mySwitch.enableTransmit(2);
   //EtekCity ZAP Remote Outlets use pulse of approximately 189
   mySwitch.setPulseLength(189);
 
-  
   // Connect to WiFi network
   Serial.println();
   Serial.println();
@@ -59,7 +55,6 @@ void setup() {
 
 void loop() {
   
-  
   // Check if a client has connected
   WiFiClient client = server.available();
   if (!client) {
@@ -79,28 +74,45 @@ void loop() {
   
   // Match the request
   int val;
-  if (req.indexOf("/sw1/0") != -1)
-    {val = 0;
-    // Insert your "OFF" code here, before ", 24)"
-    mySwitch.send(XXXXXXX, 24);}
-  else if (req.indexOf("/sw1/1") != -1)
-    {val = 1;
-    // Insert your "ON" code here, before ", 24)"
-    mySwitch.send(XXXXXXX, 24);}
-  else {
-    Serial.println("invalid request");
-    client.stop();
-    return;
+  if (req.indexOf("/sw1/0") != -1) {
+      val = 0;
+      // Insert your "OFF" code here, before ", 24)"
+      mySwitch.send(12345, 24);}
+  else if (req.indexOf("/sw1/1") != -1) {
+      val = 1;
+      // Insert your "ON" code here, before ", 24)"
+      mySwitch.send(54321, 24);
   }
-  
+  else if (req.indexOf("favicon.ico") != -1) {
+      // ignore favicon requests from browser
+      val = -1;
+  }
+  else {
+      val = -10;
+      Serial.println("invalid request");
+  }
   
   client.flush();
 
   // Prepare the response
-  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nSwitch 1 is now ";
-  s += (val)?"ON":"OFF";
-  s += "</html>\n";
-
+  // Handle (ignore) favicon requests with a 404 response
+  // https://github.com/esp8266/Arduino/issues/2080
+  String s;
+  if (val == -1){
+    s = "HTTP/1.1 404 \r\n";
+  }
+  // handle invalid requests
+  else if (val == -10){
+    s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nInvalid request! ";
+    s += "\r\nEnter URL as: \"&lt;ip-address&gt;/sw1/&lt;VAL&gt;\" where &lt;VAL&gt; is 0 or 1";
+    s += "</html>\n";
+  }
+  // correct requests
+  else{
+    s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nSwitch 1 is now ";
+    s += (val)?"ON":"OFF";
+    s += "</html>\n";
+  }
   // Send the response to the client
   client.print(s);
   delay(1);
@@ -109,4 +121,3 @@ void loop() {
   // The client will actually be disconnected 
   // when the function returns and 'client' object is detroyed
 }
-
